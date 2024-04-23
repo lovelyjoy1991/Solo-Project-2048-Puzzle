0df3581f124ff0cad15a9739de4b14aecc9f8d46
lovelyjoy1991 <50564657+lovelyjoy1991@users.noreply.github.com>
2024-04-23 18:20:07 -0400

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './client/index.js',
+  mode: process.env.NODE_ENV,
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      publicPath: '/',
+      directory: path.join(__dirname, './build'),
+    },
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:3000',
+      },
+    ],
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
+        },
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
 };
