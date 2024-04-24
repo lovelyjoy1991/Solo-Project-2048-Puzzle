cf6d78980efa0698b8cd1210900f5a6db0e1902a
lovelyjoy1991 <50564657+lovelyjoy1991@users.noreply.github.com>
2024-04-24 12:07:09 -0400

    commit again

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import Board from './components/Board';
+import Grid from './components/Grid';
+import Tile from './components/Tile';
 
-const App = () => <div id='app'>{/* rs */}</div>;
+const App = () => (
+  <div id='app'>
+    <Tile />
+    <Tile />
+    <Tile />
+  </div>
+);
 
 export default App;
---
+++ client/components/Tile.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tile = () => {
+  return <h1>HI</h1>;
+};
+
+export default Tile;
