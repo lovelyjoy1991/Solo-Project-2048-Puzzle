18b568a2937ed570f5d0ff4675aff23bbe7b1f46
lovelyjoy1991 <50564657+lovelyjoy1991@users.noreply.github.com>
2024-04-25 02:07:05 -0400

    add server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
 
-// if (process.env.NODE_ENV === 'production') {
-app.use(express.static(path.resolve(__dirname, '../build')));
-app.use(express.json());
+// // if (process.env.NODE_ENV === 'production') {
+// app.use(express.static(path.join(__dirname, '../build')));
+// app.use(express.json());
 
-app.use('/build', express.static(path.join(__dirname, '../build')));
-// serve index.html on the route '/'
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-});
-// }
-app.listen(3000);
+// app.use('/build', express.static(path.join(__dirname, '../build')));
+
+// app.get('/build/bundle.js', (req, res) => {
+//   return res.status(200).sendFile(path.join(__dirname, '../build/bundle.js'));
+// });
+// // }
+// app.listen(3000);
